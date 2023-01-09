5f86a728aa960d26ca99b9e724126ee768a8d729
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-01-08 17:35:22 -0700

    commit tree b026c22118b61ac66329746f0661e82603b560dc

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+
+  static titleize(string){
+    let myArray = string.split(" ");
+     console.log(myArray);
+    let result = [];
+    let dontChangeThoseWords = ["the","a","an","but","of","and","for","at","by","from"]
+
+      for (let i=0; i<myArray.length; i++){
+      if (i === 0){
+        result.push(this.capitalize(myArray[i]))}
+      else if (dontChangeThoseWords.includes(myArray[i])){
+        result.push(myArray[i])
+      }
+      else {
+        result.push(this.capitalize(myArray[i]))
+      }
+      }
+    
+   return result.join(" ") 
+    
+  }
 }
